5135b8d484cbc7a5f88e67dbd96f61217c721933
Chukwuma Chidiogo Winnie <chukwumachidiogo0@gmail.com>
2025-06-19 23:42:52 +0100

    Add files via upload

---
+++ the select keyword.sql
@@ -0,0 +1,10 @@
+-- Retrieve the details of all customers from mydatabase.
+
+USE mydatabase;
+SELECT * 
+FROM customers;
+
+-- Get me the order_id, order date, and sales from the order table.
+
+SELECT order_id, order_date, sales
+FROM orders;
